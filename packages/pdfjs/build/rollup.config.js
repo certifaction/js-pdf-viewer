dcbe1b1640253104aba06c578feb3e88252ee5e2
Cyrill Steinmetz <cyrill.steinmetz@certifaction.io>
2021-03-01 16:32:18 +0100

    generate pdf_viewer.scss

--- packages/pdfjs/build/rollup.config.js
+++ packages/pdfjs/build/rollup.config.js
@@ -45,7 +45,14 @@ export default [
                         src: '../../node_modules/pdfjs-dist/web/pdf_viewer.css',
                         dest: 'dist/',
                         transform: (content) => content.toString()
-                            // .replaceAll('images/', '@certifaction/pdfjs/dist/images/')
+                            .replaceAll('.pdfViewer', '.viewer-container .viewer')
+                    },
+                    {
+                        src: '../../node_modules/pdfjs-dist/web/pdf_viewer.css',
+                        dest: 'dist/',
+                        rename: (name) => `${name}.scss`,
+                        transform: (content) => content.toString()
+                            .replaceAll('images/', '@certifaction/pdfjs/dist/images/')
                             .replaceAll('.pdfViewer', '.viewer-container .viewer')
                     },
                     { src: '../../node_modules/pdfjs-dist/web/images', dest: 'dist/' },
@@ -54,56 +61,4 @@ export default [
             })
         ]
     }
-    // {
-    //     input: 'src/pdf.module.js',
-    //     output: {
-    //         file: 'dist/pdf.js',
-    //         format: 'es'
-    //     },
-    //     plugins: [
-    //         ignore(['./pdf.worker.js']),
-    //         commonjs(),
-    //         resolve({
-    //             browser: true,
-    //             preferBuiltins: false
-    //         }),
-    //         copy({
-    //             targets: [
-    //                 {
-    //                     src: [
-    //                         '../../node_modules/pdfjs-dist/build/pdf.worker.js',
-    //                         '../../node_modules/pdfjs-dist/build/pdf.worker.min.js',
-    //                         '../../node_modules/pdfjs-dist/build/pdf.worker.js.map'
-    //                     ],
-    //                     dest: 'dist/',
-    //                     rename: (name, extension) => `${name.replace('pdf.worker', 'pdfjs.worker')}.${extension}`
-    //                 },
-    //                 {
-    //                     src: '../../node_modules/pdfjs-dist/web/pdf_viewer.css',
-    //                     dest: 'dist/',
-    //                     transform: (content) => content.toString()
-    //                         // .replaceAll('images/', '@certifaction/pdfjs/dist/images/')
-    //                         .replaceAll('.pdfViewer', '.viewer-container .viewer')
-    //                 },
-    //                 { src: '../../node_modules/pdfjs-dist/web/images', dest: 'dist/' },
-    //                 { src: '../../node_modules/pdfjs-dist/cmaps', dest: 'dist/' }
-    //             ]
-    //         })
-    //     ]
-    // },
-    // {
-    //     input: 'src/pdf_viewer.module.js',
-    //     output: {
-    //         file: 'dist/pdf_viewer.js',
-    //         format: 'es'
-    //     },
-    //     plugins: [
-    //         ignore(['../build/pdf.js']),
-    //         commonjs(),
-    //         resolve({
-    //             browser: true,
-    //             preferBuiltins: false
-    //         })
-    //     ]
-    // }
 ]
