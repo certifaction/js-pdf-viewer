4896cdc35d7bc2a8204262dd7c627dc95ff539f6
Cyrill Steinmetz <3669176+bauer00@users.noreply.github.com>
2021-03-17 17:21:42 +0100

    First stable release (#1)

--- packages/vue-pdf-viewer/src/pdf/pdf.init.js
+++ packages/vue-pdf-viewer/src/pdf/pdf.init.js
@@ -1,5 +1,3 @@
 import { pdfjsLib } from '@certifaction/pdfjs'
-// import pdfjsLib from '@certifaction/pdfjs/dist/pdf.js'
-// import * as pdfjsLib from 'pdfjs-dist/build/pdf'
 
 window['pdfjs-dist/build/pdf'] = pdfjsLib
