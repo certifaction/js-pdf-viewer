9eaa865a740ad18a2f342719755af149c048bc78
alnicoli <alessandro.nicoli@certifaction.io>
2023-05-17 14:46:53 +0200

    Vue 3 updates and adjustments

--- packages/vue-pdf-viewer/src/components/PDFViewer.vue
+++ packages/vue-pdf-viewer/src/components/PDFViewer.vue
@@ -1,3 +1,151 @@
+<script setup>
+import { onMounted, ref, watch, computed, toRaw } from 'vue'
+import { mdiMinus, mdiPlus, mdiCropFree } from '@mdi/js'
+import { pdfjsLib } from '@certifaction/pdfjs'
+import pdfjsViewer from '../pdf/pdf_viewer'
+import MDIcon from './MDIcon.vue'
+
+const MIN_SCALE = 0.1
+const MAX_SCALE = 10
+
+// Props
+const props = defineProps({
+    pdfjsWorkerSrc: {
+        type: String, required: false
+    },
+    pdfjsWorkerInstance: {
+        type: Worker, required: false
+    },
+    pdfjsCMapUrl: {
+        type: String, required: true
+    },
+    source: {
+        required: true,
+        validator: (value) => {
+            return !!value && (typeof value === 'string' || value instanceof Uint8Array)
+        }
+    },
+    pdfjsViewerOptions: {
+        type: Object,
+        default: function() {
+            return {}
+        }
+    },
+    defaultScale: {
+        required: false,
+        validator: (value) => {
+            if (typeof value === 'string') {
+                return !!value && ['auto', 'page-actual', 'page-fit', 'page-width'].includes(value)
+            } else {
+                return !!value && typeof value === 'number'
+            }
+        },
+        default: 'auto'
+    }
+})
+
+// Events
+const emit = defineEmits(['error'])
+
+// Reactive data
+const state = ref({
+    pdfViewer: null,
+    pdfDocument: null,
+    currentScale: null
+})
+
+// Template refs
+const viewerControls = ref(null)
+const viewerContainer = ref(null)
+
+// Computed
+const currentPage = computed(() => state.value.pdfViewer ? state.value.pdfViewer.currentPageNumber : 0)
+const pageCount = computed(() => state.value.pdfDocument ? state.value.pdfDocument.numPages : 0)
+const showPageFitButton = computed(() => state.value.currentScale !== 'page-fit')
+
+if (props.pdfjsWorkerInstance) {
+    pdfjsLib.GlobalWorkerOptions.workerPort = props.pdfjsWorkerInstance
+} else if (props.pdfjsWorkerSrc) {
+    pdfjsLib.GlobalWorkerOptions.workerSrc = props.pdfjsWorkerSrc
+} else {
+    throw new Error('PDFViewer: pdfjsWorkerSrc or pdfjsWorkerInstance needs to be defined.')
+}
+
+function pageFit() {
+    toRaw(state.value.pdfViewer).currentScaleValue = state.value.currentScale = 'page-fit'
+}
+
+function decreaseScale() {
+    let newScale = state.value.pdfViewer.currentScale
+    newScale -= 0.1
+    newScale = newScale.toFixed(2)
+    newScale = Math.floor(newScale * 10) / 10
+
+    state.value.currentScale = (newScale < MIN_SCALE) ? MIN_SCALE : newScale
+}
+
+function increaseScale() {
+    let newScale = state.value.pdfViewer.currentScale
+    newScale += 0.1
+    newScale = newScale.toFixed(2)
+    newScale = Math.ceil(newScale * 10) / 10
+
+    state.value.currentScale = (newScale > MAX_SCALE) ? MAX_SCALE : newScale
+}
+
+// Watchers
+watch(() => state.value.currentScale, (newCurrentScale, oldCurrentScale) => {
+    if (oldCurrentScale) {
+        toRaw(state.value.pdfViewer).currentScaleValue = state.value.currentScale
+        const event = new CustomEvent('PDFViewer:scaleChange', { detail: toRaw(state.value.pdfViewer).currentScale })
+        window.dispatchEvent(event)
+    }
+})
+
+onMounted(async () => {
+    try {
+        const eventBus = new pdfjsViewer.EventBus()
+        state.value.pdfViewer = new pdfjsViewer.PDFViewer({
+            ...props.pdfjsViewerOptions,
+            container: viewerContainer.value,
+            eventBus
+        })
+
+        const docOptions = {
+            cMapUrl: props.pdfjsCMapUrl,
+            cMapPacked: true
+        }
+
+        if (props.source instanceof Uint8Array) {
+            docOptions.data = new Uint8Array(props.source)
+        } else {
+            docOptions.url = props.source
+        }
+
+        const documentLoadingTask = pdfjsLib.getDocument(docOptions)
+        state.value.pdfDocument = await documentLoadingTask.promise
+
+        const event = new Event('PDFViewer:documentLoaded')
+        window.dispatchEvent(event)
+
+        eventBus.on('pagesloaded', () => {
+            toRaw(state.value.pdfViewer).currentScaleValue = state.value.currentScale = props.defaultScale
+
+            const event = new Event('PDFViewer:pagesLoaded')
+            window.dispatchEvent(event)
+
+            const scrollbarWidth = viewerContainer.value.offsetWidth - viewerContainer.value.clientWidth
+            viewerControls.value.style.width = `calc(100% - ${scrollbarWidth}px)`
+        })
+
+        toRaw(state.value.pdfViewer).setDocument(toRaw(state.value.pdfDocument))
+    } catch (error) {
+        emit('error', error)
+        throw error
+    }
+})
+</script>
+
 <template>
     <div class="pdf-viewer">
         <div class="viewer-container" ref="viewerContainer">
@@ -5,14 +153,9 @@
         </div>
         <div class="controls" ref="viewerControls">
             <div class="pages">
-                <span class="current">{{ currentPage }}</span> {{ _$t('pdfViewer.pageOf') }} <span class="total">{{ pageCount }}</span>
+                <span class="current">{{ currentPage }}</span> {{ $t('pdfViewer.pageOf') }} <span class="total">{{ pageCount }}</span>
             </div>
             <div class="actions">
-                <div v-if="allowDocumentDownload" class="download">
-                    <div class="action-button" @click="downloadDocument">
-                        <MDIcon :icon="mdiDownload"/>
-                    </div>
-                </div>
                 <div class="scale">
                     <div v-if="showPageFitButton" class="action-button" @click="pageFit">
                         <MDIcon :icon="mdiCropFree"/>
@@ -28,196 +171,3 @@
         </div>
     </div>
 </template>
-
-<script>
-import { mdiMinus, mdiPlus, mdiDownload, mdiCropFree } from '@mdi/js'
-import { pdfjsLib } from '@certifaction/pdfjs'
-import pdfjsViewer from '../pdf/pdf_viewer'
-import i18nWrapperMixin from '../mixins/i18n-wrapper'
-import MDIcon from './MDIcon.vue'
-
-const MIN_SCALE = 0.1
-const MAX_SCALE = 10
-
-export default {
-    name: 'PDFViewer',
-    mixins: [
-        i18nWrapperMixin
-    ],
-    components: {
-        MDIcon
-    },
-    props: {
-        pdfjsWorkerSrc: {
-            type: String,
-            required: false
-        },
-        pdfjsWorkerInstance: {
-            type: Worker,
-            required: false
-        },
-        pdfjsCMapUrl: {
-            type: String,
-            required: true
-        },
-        source: {
-            required: true,
-            validator: (value) => {
-                return !!value && (typeof value === 'string' || value instanceof Uint8Array)
-            }
-        },
-        pdfjsViewerOptions: {
-            type: Object,
-            default: function() {
-                return {}
-            }
-        },
-        defaultScale: {
-            required: false,
-            validator: (value) => {
-                if (typeof value === 'string') {
-                    return !!value && ['auto', 'page-actual', 'page-fit', 'page-width'].includes(value)
-                } else {
-                    return !!value && typeof value === 'number'
-                }
-            },
-            default: 'auto'
-        },
-        documentName: {
-            type: String,
-            required: false,
-            default: 'pdf-viewer-document.pdf'
-        },
-        allowDocumentDownload: {
-            type: Boolean,
-            required: false,
-            default: false
-        },
-    },
-    data() {
-        return {
-            mdiMinus,
-            mdiPlus,
-            mdiDownload,
-            mdiCropFree,
-            pdfViewer: null,
-            pdfDocument: null,
-            currentScale: null,
-        }
-    },
-    computed: {
-        currentPage() {
-            return (this.pdfViewer) ? this.pdfViewer.currentPageNumber : 0
-        },
-        pageCount() {
-            return (this.pdfDocument) ? this.pdfDocument.numPages : 0
-        },
-        showPageFitButton() {
-            return this.currentScale !== 'page-fit'
-        }
-    },
-    watch: {
-        currentScale(newVal, oldVal) {
-            if (oldVal) {
-                this.pdfViewer.currentScaleValue = this.currentScale
-                const event = new CustomEvent('PDFViewer:scaleChange', { detail: this.pdfViewer.currentScale })
-                window.dispatchEvent(event)
-            }
-        }
-    },
-    methods: {
-        pageFit() {
-            this.pdfViewer.currentScaleValue = this.currentScale = 'page-fit'
-        },
-        decreaseScale() {
-            let newScale = this.pdfViewer.currentScale
-            newScale -= 0.1
-            newScale = newScale.toFixed(2)
-            newScale = Math.floor(newScale * 10) / 10
-
-            this.currentScale = (newScale < MIN_SCALE) ? MIN_SCALE : newScale
-        },
-        increaseScale() {
-            let newScale = this.pdfViewer.currentScale
-            newScale += 0.1
-            newScale = newScale.toFixed(2)
-            newScale = Math.ceil(newScale * 10) / 10
-
-            this.currentScale = (newScale > MAX_SCALE) ? MAX_SCALE : newScale
-        },
-        async downloadDocument() {
-            if (!this.allowDocumentDownload) {
-                return
-            }
-            if (this.source instanceof Uint8Array) {
-                this.pdfViewer.downloadManager.downloadData(this.source, this.documentName)
-            } else {
-                if (/^blob:/.test(this.source)) {
-                    const response = await fetch(this.source)
-                    if (response.status === 200) {
-                        const blob = await response.blob()
-                        this.pdfViewer.downloadManager.download(blob, this.source, this.documentName)
-                    } else {
-                        throw new Error('PDFViewer: Fetching blob url failed.')
-                    }
-                } else {
-                    this.pdfViewer.downloadManager.downloadUrl(this.source, this.documentName)
-                }
-            }
-        }
-    },
-    created() {
-        if (this.pdfjsWorkerInstance) {
-            pdfjsLib.GlobalWorkerOptions.workerPort = this.pdfjsWorkerInstance
-        } else if (this.pdfjsWorkerSrc) {
-            pdfjsLib.GlobalWorkerOptions.workerSrc = this.pdfjsWorkerSrc
-        } else {
-            throw new Error('PDFViewer: pdfjsWorkerSrc or pdfjsWorkerInstance needs to be defined.')
-        }
-    },
-    async mounted() {
-        try {
-            const eventBus = new pdfjsViewer.EventBus()
-            const downloadManager = new pdfjsViewer.DownloadManager()
-            this.pdfViewer = new pdfjsViewer.PDFViewer({
-                ...this.pdfjsViewerOptions,
-                container: this.$refs.viewerContainer,
-                eventBus,
-                downloadManager
-            })
-
-            const docOptions = {
-                cMapUrl: this.pdfjsCMapUrl,
-                cMapPacked: true
-            }
-
-            if (this.source instanceof Uint8Array) {
-                docOptions.data = new Uint8Array(this.source)
-            } else {
-                docOptions.url = this.source
-            }
-
-            const documentLoadingTask = pdfjsLib.getDocument(docOptions)
-            this.pdfDocument = await documentLoadingTask.promise
-
-            const event = new Event('PDFViewer:documentLoaded')
-            window.dispatchEvent(event)
-
-            eventBus.on('pagesloaded', () => {
-                this.pdfViewer.currentScaleValue = this.currentScale = this.defaultScale
-
-                const event = new Event('PDFViewer:pagesLoaded')
-                window.dispatchEvent(event)
-
-                const scrollbarWidth = this.$refs.viewerContainer.offsetWidth - this.$refs.viewerContainer.clientWidth
-                this.$refs.viewerControls.style.width = `calc(100% - ${scrollbarWidth}px)`
-            })
-
-            this.pdfViewer.setDocument(this.pdfDocument)
-        } catch (error) {
-            this.$emit('error', error)
-            throw error
-        }
-    }
-}
-</script>
