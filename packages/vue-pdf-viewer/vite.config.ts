caec7c42870cc23d49960b89b30505e68e74c476
Cyrill Steinmetz <cyrill.steinmetz@certifaction.com>
2024-11-07 13:16:32 +0100

    cleanup and improvements

--- packages/vue-pdf-viewer/vite.config.ts
+++
@@ -1,8 +0,0 @@
-/* eslint-env node */
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [vue()],
-})
