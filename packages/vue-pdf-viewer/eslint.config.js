95932858a6a8bbe97d212c8f87556c013fcf2941
bauer00 <3669176+bauer00@users.noreply.github.com>
2024-11-07 13:02:47 +0100

    DT-553: Move ESlint config to root & improvements (#101)

--- packages/vue-pdf-viewer/eslint.config.js
+++
@@ -1,4 +0,0 @@
-import pluginVue from 'eslint-plugin-vue'
-import skipPrettierFormatting from '@vue/eslint-config-prettier/skip-formatting'
-
-export default [...pluginVue.configs['flat/vue2-essential'], skipPrettierFormatting]
