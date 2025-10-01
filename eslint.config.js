501bb32efbc4f70bb6d25e9e8f10915b55df52ab
bauer00 <3669176+bauer00@users.noreply.github.com>
2025-10-01 12:07:32 +0200

    DT-1627: dependency updates 2025-10 (#156)

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,26 @@
+import { defineConfig } from 'eslint/config'
+import globals from 'globals'
+import js from '@eslint/js'
 import pluginVue from 'eslint-plugin-vue'
 import skipPrettierFormatting from '@vue/eslint-config-prettier/skip-formatting'
 
-export default [
+export default defineConfig([
     {
         name: 'certifaction/js-pdf-viewer/files-to-lint',
-        files: ['**/*.{js,mjs,vue}'],
+        files: ['**/*.{js,mjs,jsx,vue}'],
     },
+
+    {
+        languageOptions: {
+            globals: {
+                ...globals.browser,
+            },
+        },
+    },
+
+    js.configs.recommended,
 
     ...pluginVue.configs['flat/vue2-essential'],
 
     skipPrettierFormatting,
-]
+])
